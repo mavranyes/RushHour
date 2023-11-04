a4c72a28ffdbf2cd97d781ce50c806ce9a1c18c0
mavranyes <mavranyes@comcast.net>
2023-11-04 15:11:03 -0400

    init

--- RushHour.java
+++ RushHour.java
@@ -1,7 +1,19 @@
 class RushHour {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        char[][] map = new char[6][6];
     }
 
-    private static 
+    private static void parseInput() {
+
+    }
+
+    private static void printMap() {
+
+    }
+
+    private static void convertMaptoString() {
+
+    }
 }
