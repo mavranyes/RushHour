8484dc4c1fef204e0d0d05d15e89d219ab56cd83
Ethan-43 <hunterethan43@gmail.com>
2023-11-04 16:27:35 -0400

    x

--- RushHour.java
+++ RushHour.java
@@ -39,7 +39,7 @@ class RushHour {
     private static void printMap(String[][] map) {
         System.out.println("Rush Hour Current Representation: \n");
         System.out.println(" -------------");
-        for(int i = 0; i > 6; i++){
+        for(int i = 0; i < 6; i++){
             System.out.print((i+1) + "|");
             for(int j = 0; j < 6; j++){
                 System.out.print(map[i][j] + "|");
